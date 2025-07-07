dc715319528dc387a3572545199d406b6a478664
李志杰 <3426619342@qq.com>
2025-07-07 22:21:18 +0800

    补充

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <cstdlib>
+#include <string>
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum class Token {
+  tok_eof = -1,
+
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+
+  // primary
+  tok_identifier = -4,
+  tok_number = -5,
+};
+
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
+
+/// gettok - Return the next token from standard input.
+static Token gettok() {
+  static int LastChar = ' ';
+
+  // Skip any whitespace.
+  while (isspace(LastChar)) {
+    LastChar = getchar();
+  }
+  if (isalpha(LastChar)) {
+    // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum(LastChar = getchar())) {
+      IdentifierStr += LastChar;
+    }
+
+    if (IdentifierStr == "def") {
+      return Token::tok_def;
+    }
+    if (IdentifierStr == "extern") {
+      return Token::tok_extern;
+    }
+    return Token::tok_identifier;
+  }
+  if (isdigit(LastChar) || LastChar == '.') {
+    // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = std::strtod(NumStr.c_str(), 0);
+    return Token::tok_number;
+  }
+  if (LastChar == '#') {
+    // Comment until end of line.
+    do {
+      LastChar = getchar();
+    } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r'); // skip until end of line
+    // If we hit the end of the file, don't return EOF, just continue.
+    if (LastChar != EOF) {
+      return gettok();
+    }
+  }
+  // Check for end of file.  Don't eat the EOF.
+  if (LastChar == EOF) {
+    return Token::tok_eof;
+  }
+
+  // Otherwise, just return the character as its ascii value.
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return static_cast<Token>(ThisChar);
+}
